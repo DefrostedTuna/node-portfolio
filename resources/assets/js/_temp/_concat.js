ded435c30e680560840698e764fece25c2490334
Rick Bennett <rbennett1106@gmail.com>
2016-02-23 23:09:53 -0500

    Added contact form and partially set up functionality

--- resources/assets/js/_temp/_concat.js
+++ resources/assets/js/_temp/_concat.js
@@ -145,11 +145,83 @@ $(function () {
   $('.header-nq p').css({
     "left" : "50%",
   });
-  $('.project-arrow-right, .project-arrow-left').click(function() {
-    console.log("click");
-    $(this).toggleClass('open');
-    $(this).siblings('.project-slide-container').find('.project-slide-wrap').toggleClass('open');
+  $('.project-arrow-right').click(function() {
+    $(this).toggleClass('open-left');
+    $(this).siblings('.p-slide-wrap').find('.p-slides').toggleClass('open-left');
   });
+  $('.project-arrow-left').click(function() {
+    $(this).toggleClass('open-right');
+    $(this).siblings('.p-slide-wrap').find('.p-slides').toggleClass('open-right')
+  });
+  $('#contact-form').submit(function(e){
+    e.preventDefault();
+    // Get form fields
+    var data = {};
+    data.name     = $('#form-name').val();
+    data.email    = $('#form-email').val();
+    data.subject  = $('#form-subject').val();
+    data.reason    = $('#form-reason').val();
+    $.ajax({
+        url : "contact",
+        type: "POST",
+        data: JSON.stringify(data),
+        processData: false,
+        contentType: "application/json",
+        success: function(data){
+          console.log("SUCCESS")
+            //cleanup
+            // remove values from form
+
+            //display flash message
+            // "Email has been sent!" Or something along those lines
+
+
+        }
+    });
+  });
+
+  /*
+  $('#contact-form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+        url : "contact",
+        type: "POST",
+        data: new FormData(this),
+        processData: false,
+        contentType: false,
+        success: function(data){
+            //cleanup
+            // remove values from form
+
+            //display flash message
+            // "Email has been sent!" Or something along those lines
+
+
+        },
+
+        error: function(jqXHR, textStatus, errorThrown){
+            //cleanup
+            $(".flash-error").remove();
+            $(".selected-file").remove();
+            $(".button-file-select").removeClass("button-file-select-on");
+            //prepend div to top of page
+            $("<div class='flash-error'></div>").prependTo(".main-wrap").hide();
+            $.each($.parseJSON(jqXHR.responseText),function(index, array){
+                $.each(array, function(i, error){
+                    //add each error to the div
+                    $("<p>" + error + "</p>").appendTo(".flash-error");
+                });
+            });
+            //show flash-errors
+            $(".flash-error").fadeIn(1000);
+        }
+    });
+
+
+
+
+
+  */
 });
 
 (function() {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(process.env.TEST_VAR);
+  res.render('index');
+});
+
+router.post('/contact', function(req, res, next) {
+  console.log(process.env.TEST_VAR);
+  console.log(req.body);
+  console.log(process.env.EMAIL_USERNAME);
+  console.log(process.env.EMAIL_PASSWORD);
+
+  var smtpConfig = {
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+      }
+  };
+  var transporter = nodemailer.createTransport(smtpConfig);
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: '"Uptilt Contact" <RBennett1106@gmail.com>', // sender address
+      to: 'uptilt.io@gmail.com', // list of receivers
+      subject: req.body.subject, // Subject line
+      html: '<h1>' + req.body.name + ' just submitted a contact request on uptilt.io</h1>' +
+            '<p>Message reads as follows:</p>' +
+            '<p>' + req.body.reason + '</p>' +
+            '<p> Response email is listed as ' + req.body.email + '</p>',// html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+  });
 });
 
 module.exports = router;
